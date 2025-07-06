0e5df1871a4afc4895b4301ff7ad068351546e4b
daisuke0316 <dshengtian6@gmail.com>
2025-07-06 20:03:10 +0900

    バージョン２

--- utils.py
+++ utils.py
@@ -38,7 +38,7 @@ class Utils:
             # 日付変換
             today = datetime.strptime(getdata["取得日時"], "%Y/%m/%d")
             created = datetime.strptime(getdata["アカウント作成日"], "%Y/%m/%d")
-
+            enriched["取得日時"]=today
             # 動画 / 月（K列）
             month_diff = (today.year - created.year) * 12 + (today.month - created.month)
             enriched["動画 / 月"] = round(getdata["動画数"] / month_diff, 2) if month_diff > 0 else 0
